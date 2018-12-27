90024e08b250ca8e691cf8cb7f323023176b7d42
Omar Saber <omar.saber@ligadigital.com>
2018-12-27 10:47:36 +0100

    Add explicit types for public functions

--- exercises/src/main/scala/fpinscala/parallelism/Par.scala
+++ exercises/src/main/scala/fpinscala/parallelism/Par.scala
@@ -12,7 +12,7 @@ object Par {
   
   private case class UnitFuture[A](get: A) extends Future[A] {
     def isDone = true 
-    def get(timeout: Long, units: TimeUnit) = get 
+    def get(timeout: Long, units: TimeUnit): A = get
     def isCancelled = false 
     def cancel(evenIfRunning: Boolean): Boolean = false 
   }
@@ -26,13 +26,13 @@ object Par {
   
   def fork[A](a: => Par[A]): Par[A] = // This is the simplest and most natural implementation of `fork`, but there are some problems with it--for one, the outer `Callable` will block waiting for the "inner" task to complete. Since this blocking occupies a thread in our thread pool, or whatever resource backs the `ExecutorService`, this implies that we're losing out on some potential parallelism. Essentially, we're using two threads when one should suffice. This is a symptom of a more serious problem with the implementation, and we will discuss this later in the chapter.
     es => es.submit(new Callable[A] { 
-      def call = a(es).get
+      def call: A = a(es).get
     })
 
   def map[A,B](pa: Par[A])(f: A => B): Par[B] = 
     map2(pa, unit(()))((a,_) => f(a))
 
-  def sortPar(parList: Par[List[Int]]) = map(parList)(_.sorted)
+  def sortPar(parList: Par[List[Int]]): Par[List[Int]] = map(parList)(_.sorted)
 
   def equal[A](e: ExecutorService)(p: Par[A], p2: Par[A]): Boolean = 
     p(e).get == p2(e).get
